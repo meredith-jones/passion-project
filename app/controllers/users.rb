99ab339db5d30c8c89a0f72c2ca6789d27a86b40
meredith-jones <meredithjw@gmail.com>
2016-11-04 20:45:27 -0700

    Add registration form and complete users post route, add error handling for user registration.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,11 +1,22 @@
 # Register
 get '/users/new' do
   # generate form for user registration
+  @user = User.new
+  erb :'users/new'
 end
 
 post '/users' do
   # submit new user form and create user
-
+  user = User.create(params[:user])
+  if user.valid?
+    # success
+    redirect '/'
+  else
+    # failure
+    status 422
+    @errors = user.errors.full_messages
+    erb :'users/new'
+  end
 end
 
 # Log in
