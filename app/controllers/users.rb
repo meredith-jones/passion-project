9032df67184f5725b11d0a9e2c6c53a5729499b7
meredith-jones <meredithjw@gmail.com>
2016-11-04 21:44:31 -0700

    Implement current_user helper, add login and logout functionality to views and controllers.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -9,10 +9,9 @@ post '/users' do
   # submit new user form and create user
   user = User.create(params[:user])
   if user.valid?
-    # success
+    session[:user_id] = user.id
     redirect '/'
   else
-    # failure
     status 422
     @errors = user.errors.full_messages
     erb :'users/new'
@@ -22,13 +21,26 @@ end
 # Log in
 get '/sessions/new' do
  # new action
+ erb :'sessions/new'
 end
 
 post '/sessions' do
   # create session
+  user = User.authenticate(params[:user][:username], params[:user][:password])
+  if user
+    session[:user_id] = user.id
+    p session
+    redirect '/'
+  else
+    status 422
+    @errors = ["Login failed"]
+    erb :'sessions/new'
+  end
 end
 
 # Log out
-delete '/sessions/:id' do
+delete '/logout' do
   # delete session
+  session.delete(:user_id)
+  redirect '/'
 end
