aaa31d21f59deb72821900ff58510d845da77652
meredith-jones <meredithjw@gmail.com>
2016-11-05 13:24:26 -0700

    Add show and edit views and routes for symptoms, edit not complete but form is in place.

--- app/controllers/symptoms.rb
+++ app/controllers/symptoms.rb
@@ -7,21 +7,32 @@ end
 
 get '/symptoms/new' do
   # return an HTML form for creating a new symptom
-  # erb :'/symptoms/new'
+  @symptom = Symptom.new
+  erb :'symptoms/new'
 end
 
 post '/symptoms' do
   # create a new symptom
+  symptom = Symptom.create(params[:symptom])
+  if symptom.valid?
+    current_user.symptoms << symptom
+    redirect '/symptoms'
+  else
+    @errors = symptom.errors.full_messages
+    erb :'symptoms/new'
+  end
 end
 
 get '/symptoms/:id' do
   # display a specific symptom
-  # erb :'/symptoms/show'
+  @symptom = Symptom.find(params[:id])
+  erb :'symptoms/show'
 end
 
 get '/symptoms/:id/edit' do
   # return an HTML form for editing a symptom
-  # erb :'/symptoms/edit'
+  @symptom = Symptom.find(params[:id])
+  erb :'symptoms/edit'
 end
 
 put '/symptoms/:id' do
