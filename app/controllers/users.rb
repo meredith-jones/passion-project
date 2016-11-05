3613c2950798ae59dc3650ed5c677032a6a2e767
meredith-jones <meredithjw@gmail.com>
2016-11-04 20:13:31 -0700

    Add skeleton for users controller.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,23 @@
 # Register
+get '/users/new' do
+  # generate form for user registration
+end
+
+post '/users' do
+  # submit new user form and create user
+
+end
 
 # Log in
+get '/sessions/new' do
+ # new action
+end
+
+post '/sessions' do
+  # create session
+end
 
 # Log out
-
+delete '/sessions/:id' do
+  # delete session
+end
