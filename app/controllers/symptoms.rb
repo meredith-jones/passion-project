e0c280415ddb185fa4c45165c46c4e5ea53839fb
meredith-jones <meredithjw@gmail.com>
2016-11-07 20:35:03 -0800

    AJAX view and add symptoms partials.

--- app/controllers/symptoms.rb
+++ app/controllers/symptoms.rb
@@ -2,21 +2,28 @@ get '/symptoms' do
   # display list of all symptoms
   # erb :'/symptoms/index'
   @symptoms = current_user.symptoms
-  erb :'symptoms/list'
+  erb :'symptoms/_list', layout: false, locals: {symptoms: @symptoms }
 end
 
 get '/symptoms/new' do
   # return an HTML form for creating a new symptom
   @symptom = Symptom.new
-  erb :'symptoms/new'
+  erb :'symptoms/new', layout: false, locals: {symptom: @symptom}
 end
 
 post '/symptoms' do
   # create a new symptom
   symptom = Symptom.create(params[:symptom])
-  if symptom.valid?
+  if request.xhr? && symptom.valid?
     current_user.symptoms << symptom
-    redirect '/symptoms'
+    @symptoms = current_user.symptoms
+    erb :'symptoms/_list', layout: false, locals: {symptoms: @symptoms }
+  elsif symptom.valid?
+    current_user.symptoms << symptom
+    redirect '/'
+  elsif request.xhr? && !symptom.valid?
+    @errors = symptom.errors.full_messages
+    erb :'symptoms/new', layout: false, locals: {symptom: symptom}
   else
     @errors = symptom.errors.full_messages
     erb :'symptoms/new'
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,55 @@
 $(document).ready(function() {
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
+  viewSymptoms();
+  viewSymptomForm();
+  addSymptom();
 });
+
+function viewSymptoms(){
+    $("#view-symptoms").on("click", "a", function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "GET",
+      url: "/symptoms"
+    })
+    .done(function(response){
+      $("#user-info-viewer").empty();
+      $("#user-info-viewer").append(response);
+    })
+  })
+}
+
+function viewSymptomForm(){
+    $("#new-symptom").on("click", "a", function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "GET",
+      url: "/symptoms/new"
+    })
+    .done(function(response){
+      $("#user-info-viewer").empty();
+      $("#user-info-viewer").append(response);
+    })
+  })
+}
+
+function addSymptom(){
+  $("#user-info-viewer").on("click", "#submit-symptom", function(event){
+    event.preventDefault();
+    var button = $(this);
+    var form = $(button.parent());
+    var data = form.serialize();
+    console.log(data);
+  $.ajax({
+    url: form.attr("action"),
+    method: form.attr("method"),
+    data: data
+  })
+  .done(function(response){
+    // console.log(response);
+    $("#user-info-viewer").empty();
+    $("#user-info-viewer").append(response);
+  })
+
+  })
+
+}
