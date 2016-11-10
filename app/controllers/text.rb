c94d3e3d8c9e3f7ff80f37eb9cf5c8728e33ad56
meredith-jones <meredithjw@gmail.com>
2016-11-10 12:28:41 -0800

    Comment out twilio stuff since it broke everything.

--- app/controllers/text.rb
+++ app/controllers/text.rb
@@ -1,34 +1,34 @@
-require 'twilio-ruby'
-require 'sinatra'
+# require 'twilio-ruby'
+# require 'sinatra'
 
 
-@client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+# @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
-@client.account.messages.create(
-    from: '+12062021955',
-    to: '+13103652965',
-    body: 'Hide your keys.'
-  )
+# @client.account.messages.create(
+#     from: '+12062021955',
+#     to: '+13103652965',
+#     body: 'Hide your keys.'
+#   )
+
+# # post '/text' do
+# #   puts "Message #{params['Body']}"
+
+# #   # twiml = Twilio::TwiML::Response.new do |r|
+# #   #   r.Message "Symptom added!"
+# #   # end
+# #   # twiml.txt
+
+# # end
 
 # post '/text' do
-#   puts "Message #{params['Body']}"
+#   number = params['From']
+#   body = params['Body']
 
-#   # twiml = Twilio::TwiML::Response.new do |r|
-#   #   r.Message "Symptom added!"
-#   # end
-#   # twiml.txt
+#   content_type 'text/xml'
 
+#   "<Response>
+#     <Message>
+#       Hello #{number}. You said: #{body}
+#     </Message>
+#   </Response>"
 # end
-
-post '/text' do
-  number = params['From']
-  body = params['Body']
-
-  content_type 'text/xml'
-
-  "<Response>
-    <Message>
-      Hello #{number}. You said: #{body}
-    </Message>
-  </Response>"
-end
